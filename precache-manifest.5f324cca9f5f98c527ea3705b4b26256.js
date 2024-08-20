4e6424b198481f75a50cddb7331414134c1817d4
rajvora <rajvora1998@gmail.com>
2024-08-20 11:38:18 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.5f324cca9f5f98c527ea3705b4b26256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d4f7477a9444a3dae356424474b717",
+    "url": "/2048-react/index.html"
+  },
+  {
+    "revision": "75c224de1c9ad9fa5edf",
+    "url": "/2048-react/static/js/2.0f057667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048-react/static/js/2.0f057667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ecbb0abc593e855665",
+    "url": "/2048-react/static/js/main.baa495bc.chunk.js"
+  },
+  {
+    "revision": "960fc470755ce94b8f1e",
+    "url": "/2048-react/static/js/runtime-main.57db5a22.js"
+  }
+]);
